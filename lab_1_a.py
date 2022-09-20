8e55c8e4fa2ec8d5fed21737cac2c123dbf38c8d
Shreyam Pokharel <pshreyam@gmail.com>
2022-09-20 09:40:30 +0545

    Update lab 1 a to generate tokens
    Currently checks lexemes using empty spaces as delimeter

--- lab_1_a.py
+++ lab_1_a.py
@@ -1,41 +1,125 @@
-import os
+class Lexer(object):
+    def __init__(self, filename):
+        self.contents = self.read_file(filename)
+        self.keywords = [
+            "def",
+            "if",
+            "return",
+            "True",
+            "False",
+        ]
+        self.operators = {
+            "=": "<assignop>",
+            "+": "<addop>",
+            "-": "<subop>",
+            "*": "<multop>",
+            "/": "<divop>",
+            "==": "<eqop>",
+            ">": "<gtop>",
+            "<": "<ltop>",
+            ">=": "<gteop>",
+            "<=": "<lteop>",
+        }
+        self.symbols = [
+            "!",
+            "\"",
+            "#",
+            "$",
+            "%",
+            "&",
+            "'",
+            "(",
+            ")",
+            "*",
+            "+",
+            ",",
+            "-",
+            ".",
+            "/",
+            ":",
+            ";",
+            "<",
+            "=",
+            ">",
+            "?",
+            "@",
+            "[",
+            "\\",
+            "]",
+            "^",
+            "_",
+            "`",
+            "{",
+            "|",
+            "}",
+            "~"]
+        self.symbol_table = []
 
-filename = "code.txt"
+    def read_file(self, filename):
+        with open(filename, "r") as f:
+            content = f.read()
+        return list(content)
+
+    def is_integer(self, lexeme):
+        try:
+            if all([x in range(0, 10)
+                   for x in map(lambda a: int(a), list(lexeme))]):
+                return True
+            return False
+        except BaseException:
+            return False
+
+    def tokenize(self):
+        line_number = 1
+        current_lexeme = ""
+        for index, character in enumerate(self.contents):
+            if character == " " or character == "\n":
+                if character == "\n":
+                    line_number += 1
+                current_lexeme = ""
+            elif character in ":(),":
+                print(f">> {character}")
+                self.symbol_table.append(
+                    Token(
+                        character,
+                        "SPECIAL_SYMBOL",
+                        line_number))
+                current_lexeme = ""
+            elif character in self.operators:
+                current_lexeme += character
+                if current_lexeme + \
+                        self.contents[index + 1] not in self.operators:
+                    print(f">> {current_lexeme}")
+                    self.symbol_table.append(
+                        Token(current_lexeme, "OPERATOR", line_number))
+                    current_lexeme = ""
+            else:
+                current_lexeme += character
+                if current_lexeme in self.keywords:
+                    self.symbol_table.append(
+                        Token(current_lexeme, "KEYWORD", line_number))
+                    print(">>", current_lexeme)
+                else:
+                    if index <= len(self.contents) - 2:
+                        if self.contents[index + 1] in list("(), :\n"):
+                            if not current_lexeme in " \n":
+                                self.symbol_table.append(
+                                    Token(current_lexeme, "IDENTIFIER", line_number))
+                                print(">>", current_lexeme)
 
 
-def read_file(filename):
-    with open(filename, "r") as f:
-        content = f.read()
+class Token(object):
+    def __init__(self, lexeme, token_type, line_number):
+        self.lexeme = lexeme
+        self.token_type = token_type
+        self.line_number = line_number
 
-    return list(content)
+    def __repr__(self):
+        return f"<{self.token_type}\t\t'{self.lexeme}'\t\tLine:{self.line_number}>"
 
 
-buffer = read_file(filename)
-
-keywords = ["import", "def", "print"]
-
-operators = {
-    "=" : "<assignop>",
-    "+" : "<addop>",
-    "-" : "<subop>",
-    "*" : "<multop>",
-    "/" : "<divop>",
-    "==": "<eqop>",
-    ">" : "<gtop>",
-    "<" : "<ltop>",
-    ">=": "<gteop>",
-    "<=": "<lteop>",
-}
-
-identifiers = ["keywords", "operators"]
-
-# print(buffer)
-
-current_lexeme = []
-
-# for i in range(len(buffer)):
-#     current_lexeme.append(buffer[i])
-#     if "".join(current_lexeme) in keywords:
-#         if "".join([*current_lexeme, buffer[i+1]]) not in keywords:
-#             print("Keyword")
-#             current_lexeme = []
+if __name__ == "__main__":
+    lexer = Lexer("code.txt")
+    lexer.tokenize()
+    for index, token in enumerate(lexer.symbol_table):
+        print(index + 1, token)
