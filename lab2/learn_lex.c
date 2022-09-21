9fb660143852f824497b3ebf0afd3ce161e5048f
Shreyam Pokharel <pshreyam@gmail.com>
2022-09-21 06:38:22 +0545

    Add first lex code
    Compiling with gcc yields lex.yy.c

---
+++ lab2/learn_lex.c
@@ -0,0 +1,19 @@
+/* Learning lex using C */
+
+/* Definitions */
+%{
+
+%}
+
+/* Rules */
+%%
+%%
+
+/* User Subroutines */
+int ywrap(){}
+
+int main(int argc, char **argv) {
+    /* actual work */
+    yylex();
+    /* actual work */
+}
