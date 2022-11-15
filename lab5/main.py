ecd57e2d84455a130a94dd6fa6b207f4af1dfd3c
Shreyam Pokharel <pshreyam@gmail.com>
2022-11-15 22:28:41 +0545

    Update lab5 to handle complex grammar

--- lab5/main.py
+++ lab5/main.py
@@ -29,14 +29,17 @@ def left_factor(rules):
                 for another_term in right_side:
                     if not term == another_term:
                         longest_prefix = find_longest_prefix(term, another_term)
-                        if longest_prefix == "":
-                            continue
-                        longest_prefices.append(longest_prefix)
-
-        prefix = ""
-        for longest_prefix in longest_prefices:
-            if len(longest_prefix) > len(prefix):
-                prefix = longest_prefix
+                        if not longest_prefix == "":
+                            if len(longest_prefices) > 0:
+                                for i in range(len(longest_prefices)):
+                                    if longest_prefices[i].startswith(longest_prefix) or longest_prefix.startswith(longest_prefices[i]):
+                                        if len(longest_prefix) < len(longest_prefices[i]):
+                                            longest_prefices[i] = longest_prefix
+                                    else:
+                                        if longest_prefix not in longest_prefices:
+                                            longest_prefices.append(longest_prefix)
+                            else:
+                                longest_prefices.append(longest_prefix)
 
         # If α ≠ ε, replace all of the A-productions A → α β1 | α β2 |….| α βn |γ,
         # where γ represents all alternatives that do not begin with α, by
@@ -44,28 +47,38 @@ def left_factor(rules):
         # A → α A' | γ
         # A' → β1 | β2 |….| βn
 
-        terms_with_prefix = []
-        terms_without_prefix = []
-        if len(prefix) > 0:
+        if len(longest_prefices) > 0:
+            new_rules = []
+            right_side_rules = []
+
+            for prefix in longest_prefices:
+                terms_with_prefix = list()
+                for term in right_side:
+                    if term.startswith(prefix):
+                        new_term = term[len(prefix):]
+                        if new_term == "":
+                            new_term = "ε"
+                        terms_with_prefix.append(new_term)
+                B = A
+                while any([x.startswith(B) for x in rules]) or any([x.startswith(B) for x in new_rules]):
+                    B += "'"
+                right_side_rules.append(f"{prefix}{B}")
+                new_rules.append(f"{B} -> {' | '.join([term for term in terms_with_prefix])}")
+
+            # Terms without common prefix
+            gamma = []
+
             for term in right_side:
-                if term.startswith(prefix):
-                    new_term = term.replace(prefix, "")
-                    if new_term == "":
-                        new_term = "ε"
-                    terms_with_prefix.append(new_term)
-                else:
-                    terms_without_prefix.append(term)
-            B = A
-            while any([x.startswith(B) for x in rules]):
-                print("Rules: ", rules)
-                B += "'"
-            rule1 = f"{A} -> {prefix}{B}"
-            if len(terms_without_prefix) > 0:
-                rule1 += " | " + " | ".join(terms_without_prefix)
-            rule2 = f"{B} -> {' | '.join([term for term in terms_with_prefix])}"
-            rules.append(rule1)
-            rules.append(rule2)
+                if not any([term.startswith(p) for p in longest_prefices]):
+                    gamma.append(term)
+
+            modified_rule = f"{A} -> {' | '.join(right_side_rules)}"
+            if len(gamma) > 0:
+                modified_rule += " | " + " | ".join(gamma)
+            rules.append(modified_rule)
+            rules.extend(new_rules)
             rules.remove(rule)
+
     return rules
 
 
